48a452826daec97b5ac6c07144624e3a54d7d9c7
Cars-n <104526149+Cars-n@users.noreply.github.com>
2023-12-03 14:48:21 -0500

    changes made to booking. started menu and lodging pages
    Co-Authored-By: Nassim Marhari <92116345+nmarhari@users.noreply.github.com>
    Co-Authored-By: mshreff4 <129811509+mshreff4@users.noreply.github.com>
    Co-Authored-By: gabemeros <117417736+gabemeros@users.noreply.github.com>

---
+++ js/bookingImages.js
@@ -0,0 +1,39 @@
+document.getElementById('cabin-select').addEventListener('change', function () {
+    // Get the selected option
+    var selectedCabin = this.value;
+
+    // Update the large image and total price based on the selected cabin
+    updateCabinInfo(selectedCabin);
+});
+
+function updateCabinInfo(selectedCabin) {
+    var cabinImages = document.querySelectorAll('.cabin-image');
+    var totalElement = document.getElementById('total');
+
+    // Hide all images by default
+    cabinImages.forEach(function (image) {
+        image.style.display = 'none';
+    });
+
+    // Update image and total price based on the selected cabin
+    switch (selectedCabin) {
+        case 'dreamcatcher':
+            document.getElementById('dreamcatcherImage').style.display = 'inline-block';
+            totalElement.textContent = 'Your total is: $85.00';
+            break;
+        case 'rising-sun':
+            document.getElementById('risingSunImage').style.display = 'inline-block';
+            totalElement.textContent = 'Your total is: $175.00';
+            break;
+        case 'magnolia':
+            document.getElementById('magnoliaImage').style.display = 'inline-block';
+            totalElement.textContent = 'Your total is: $200.00';
+            break;
+        case 'rockpath':
+            document.getElementById('rockpathImage').style.display = 'inline-block';
+            totalElement.textContent = 'Your total is: $250.00';
+            break;
+        default:
+            totalElement.textContent = '';
+    }
+}
